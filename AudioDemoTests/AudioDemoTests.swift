54e7b27cda53c39a74092b648370e6afacc433c0
Kevin Truong <wozzack.lu@gmail.com>
2025-09-08 23:03:10 -0700

    starting test suite implementation

---
+++ AudioDemoTests/AudioDemoTests.swift
@@ -0,0 +1,28 @@
+//
+//  AudioDemoTests.swift
+//  AudioDemoTests
+//
+//  Created by Kevin Truong on 9/8/25.
+//
+//
+import Testing
+@testable import AudioDemo
+
+class AudioManagerTestSuite {
+    init() {
+        print("Starting AudioManager Test Suite.")
+    }
+    
+    deinit {
+        print("Ending AudioManager Test Suite.")
+    }
+    // need tests for seeking bar, playlist, loading audio, and convert helper functions
+    
+    @Test func
+}
+
+
+
+@Test func AudioManagerTest() {
+    #expect(true == true, "True should be equal to true")
+}
